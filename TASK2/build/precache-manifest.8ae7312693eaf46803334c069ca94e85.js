0dae13427016e7ecef4177403187e2697e17df38
POONAM JADHAV <111104545+POONAMJADHAV65@users.noreply.github.com>
2022-12-11 11:52:24 +0530

    Add files via upload

---
+++ TASK2/build/precache-manifest.8ae7312693eaf46803334c069ca94e85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "07e6837f55a2ee66e137",
+    "url": "/static/css/main.08f03a8c.chunk.css"
+  },
+  {
+    "revision": "07e6837f55a2ee66e137",
+    "url": "/static/js/main.07e6837f.chunk.js"
+  },
+  {
+    "revision": "002a2879119e0b5d31fb",
+    "url": "/static/js/1.002a2879.chunk.js"
+  },
+  {
+    "revision": "256e7c1a905ff4b677bd",
+    "url": "/static/js/2.256e7c1a.chunk.js"
+  },
+  {
+    "revision": "3220869980800ef67035",
+    "url": "/static/js/runtime~main.32208699.js"
+  },
+  {
+    "revision": "051bfb5fc8c617c5620d2535401a40fc",
+    "url": "/index.html"
+  }
+];
